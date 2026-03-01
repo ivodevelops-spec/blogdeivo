526de41525c892bae1c3f4d2973321f7107a8ec2
Daniil Poletaev <44584010+danpoletaev@users.noreply.github.com>
2026-03-01 13:31:09 +0100

    fix: move openapi generate to core

--- config/initializers/apipie.rb
+++
@@ -1,9 +0,0 @@
-Apipie.configure do |config|
-  config.app_name                = "BlogBowl API"
-  config.api_base_url            = "/api/v1"
-  config.doc_base_url            = "/apidoc"
-  config.app_info                = "REST API for managing blog pages, posts, authors, newsletters, and subscribers."
-  config.api_controllers_matcher = ["#{Rails.root}/submodules/core/app/controllers/api/v1/**/*.rb"]
-  config.validate                = false
-  config.generator.swagger.content_type_input = :json
-end
